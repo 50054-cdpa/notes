f79f501d87c7690c045ff35731084a70d3841313
Divy <divy1211.dc@gmail.com>
2024-09-08 17:40:03 +0800

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+set -e
+
 current_branch=$(git branch --show-current)
 
 mkdocs build
 git stash
-git switch -c gh-pages
-cp -rf ./site ./
+git switch -C gh-pages
+find . -maxdepth 1 ! -name ".git" ! -name "." ! -name "site" -exec rm -rf {} \;
+cp -rf ./site/* ./
 rm -rf ./site
-git commit -m "built and deployed docs manually"
-git push
+git add -A
+if git diff --cached --quiet; then
+  echo "No changes to commit"
+else
+  git commit -m "built and deployed docs manually"
+  git push
+fi
 git checkout "$current_branch"
 git stash pop
