2535814ef908a8ade0d6e4b4a6a1036e0fa465f3
Divy <divy1211.dc@gmail.com>
2024-09-08 16:45:18 +0800

    added requirements.txt

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
+current_branch=$(git branch --show-current)
+
 mkdocs build
 git stash
-git checkout gh-pages
-git pull
+git switch -c gh-pages
 cp -rf ./site ./
 rm -rf ./site
 git commit -m "built and deployed docs manually"
 git push
-git checkout main
+git checkout "$current_branch"
 git stash pop
